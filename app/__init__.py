3e33ae51aba0b4dae7f24f1dd1d7686f06d40333
David Ficociello <davidfic@skelmir.com>
2013-04-12 14:45:08 -0400

    a few minor updates and adding a .gitignore file

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,17 @@ from flask import Flask
 from flask.ext.debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config.update(
-    DEBUG_TB_ENABLED=True,
-    SECRET_KEY="BLAH"
+# app.config.update(
+#     DEBUG_TB_ENABLED=True,
+#     SECRET_KEY="BLAH"
 
-    )
+#     )
 # app = Flask('Caffeine')
 from app import views
 
 # def debug():
 # 	assert current_app.debug == False
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 def get_resource_as_string(name, charset='utf-8'):
     with app.open_resource(name) as f:
         return f.read().decode(charset)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, send_file
 from app import app
 import os
-import re
 
 chips = {
 	"i686": "i686",
@@ -27,29 +26,26 @@ def all_images(image_path):
 	for root, dirs, files in os.walk(os.path.join(cur_dir,image_path)):
 		for file in files:
 			file = os.path.join(root, file)
-			app.logger.debug(file)
+			#app.logger.debug(file)
 			yield strip(file)
-			
-
 
 def get_images(image_path, chip, config):
 	for file in all_images(image_path):
-		app.logger.debug(image_path)
+		#app.logger.debug(image_path)
 		conf_cpu = "%s-%s" % (confs.get(config), chip)
-		if conf_cpu in file:	
-			app.logger.debug(file)
+		if conf_cpu in file:
+			#app.logger.debug(file)
 			yield file
-	
+
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html", 
+	return render_template("index.html",
 		title = 'CaffeineMark Results')
 
 @app.route('/static/images/caffeine<path:filename>')
 def custom_static(filename):
 	return send_file(filename)
-	
 
 @app.route('/caffeine')
 @app.route('/caffeine-<chip>/')
@@ -60,14 +56,15 @@ def caffeinemips(chip=None,configuration=None):
 			title = "All the CaffeineMark Numbers",
 			files = all_images('static/images/caffeine'),configuration="",cpu="All")
 	elif not configuration:
+		app.logger.debug(chips.get(chip))
 		return render_template("caffeine.html",
 			title = "All the " + chip + " Numbers",
-			files = all_images('static/images/caffeine'),configuration="All",chip=chip)
-			
+			files = all_images('static/images/caffeine'),configuration="All",cpu=chips.get(chip,"Unknown"))
+
 	return render_template("caffeine.html",
-		title = 'Caffeine Mark numbers', 
+		title = 'Caffeine Mark numbers',
 		files=get_images('static/images/caffeine/',chip,configuration),
-		configuration=confs.get(configuration,"unknown"), cpu=chips.get(chip,"unknown"),test="test")
+		configuration=confs.get(configuration,"unknown"), cpu=chips.get(chip,"unknown"))
 
 
 
